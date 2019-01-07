b52d9ad60ee8c89d66317ad7a6905c921aae8a4e
László Horváth <bedafoltyn@gmail.com>
2019-01-07 22:05:37 +0100

    populate all functoin added to Population module

--- lib/db/population/population.ex
+++ lib/db/population/population.ex
@@ -4,11 +4,16 @@ defmodule Db.Population do
   alias Db.{Repo, Schema}
   alias Db.Service.{Site, ArchiveSelector, ArticleSelector, Category}
 
+  def populate_all() do
+    populate_sites()
+    populate_categories()
+  end
+
   def populate_sites() do
     Repo.delete_all(Schema.ArchiveSelector)
     Repo.delete_all(Schema.ArticleSelector)
     Repo.delete_all(Schema.Site)
-    
+
     "lib/db/population/data/sites.json"
     |> File.read!
     |> Jason.decode!
