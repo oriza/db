6cc334af57d935d2a56e7d5cca9ab5a96077cd36
László Horváth <bedafoltyn@gmail.com>
2019-04-05 20:45:28 +0200

    did_grow logic added to rate update

--- lib/db/rate/service.ex
+++ lib/db/rate/service.ex
@@ -17,8 +17,10 @@ defmodule Db.Rate.Service do
   end
 
   def update(%Rate{} = rate, attrs) do
+    attrs_with_grow = attrs |> Map.put(:did_grow, did_grow(rate, attrs))
+
     rate
-    |> Rate.changeset(attrs)
+    |> Rate.changeset(attrs_with_grow)
     |> Repo.update()
   end
 
@@ -28,5 +30,16 @@ defmodule Db.Rate.Service do
 
   def change(%Rate{} = rate) do
     Rate.changeset(rate, %{})
+  end
+
+  defp did_grow(old_rate, new_rate) do
+    cond do
+      old_rate.value == new_rate.value ->
+        old_rate.did_grow
+      old_rate.value > new_rate.value ->
+        true
+      old_rate.value < new_rate.value ->
+        false
+    end
   end
 end
